8670102b65a4275e8405cec0b96f1d3a8f0e8f4c
Praisecodes <cmancoder@gmail.com>
2023-07-21 20:28:40 +0100

    [Update] Build the empty screens for team and board screens

--- src/app/(user_area)/board/page.tsx
+++ src/app/(user_area)/board/page.tsx
@@ -1,6 +1,41 @@
+'use client'
+
+import { Empty } from "@/components/templates";
+import { DLink, Button } from "@/components/atoms";
+import { AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
+
 const Page = ():JSX.Element => {
+  const [boards, setBoards] = useState<any[]>([]);
+
   return (
-    <section></section>
+    <main className={`flex flex-col`}>
+      <section className={`flex justify-between items-center`}>
+        <div>
+          <h2 className={`text-2xl text-white`}>
+            Task Board
+          </h2>
+          <p className={`text-[#ffffff66]`}>
+            Create a board and complete tasks using board
+          </p>
+        </div>
+
+        <Button icon={<AiOutlinePlus />} onClick={() => { }} disabled={false}>
+          Create Board
+        </Button>
+      </section>
+
+      <section className={`min-h-[50vh] flex ${boards.length < 1 && 'items-center' } my-2 justify-center`}>
+        {
+          boards.length < 1 ?
+            <Empty title="No board has been created" image="/images/emptys/no-board.svg">
+              Create a board using <DLink href={'/'}>+Create Board</DLink> button
+            </Empty>
+            :
+            (null)
+        }
+      </section>
+    </main>
   )
 }
 
--- src/app/(user_area)/team/page.tsx
+++ src/app/(user_area)/team/page.tsx
@@ -1,6 +1,41 @@
-const Page = ():JSX.Element => {
+'use client'
+
+import { Button, DLink } from "@/components/atoms";
+import { Empty } from "@/components/templates";
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const Page = (): JSX.Element => {
+  const [teamList, setTeamList] = useState<any[]>([]);
+
   return (
-    <section></section>
+    <main className={`flex flex-col`}>
+      <section className={`flex justify-between items-center`}>
+        <div>
+          <h2 className={`text-2xl text-white`}>
+            My Team
+          </h2>
+          <p className={`text-[#ffffff66]`}>
+            0 Total Teams are Added
+          </p>
+        </div>
+
+        <Button icon={<AiOutlinePlus />} onClick={() => { }} disabled={false}>
+          Team
+        </Button>
+      </section>
+
+      <section className={`min-h-[50vh] flex ${teamList.length < 1 && 'items-center' } my-2 justify-center`}>
+        {
+          teamList.length < 1 ?
+            <Empty title="No teams has been created" image="/images/emptys/no-teams.svg">
+              Click to add <DLink href={'/'}>New team</DLink>
+            </Empty>
+            :
+            (null)
+        }
+      </section>
+    </main>
   )
 }
 
