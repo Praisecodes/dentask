c3cad7b7316d4d374e4a3ed4894a887facb1d50c
Praisecodes <cmancoder@gmail.com>
2023-09-15 10:50:55 +0100

    [Update] Build profile modal

--- src/components/molecules/header_actions.tsx
+++ src/components/molecules/header_actions.tsx
@@ -12,14 +12,14 @@ const HeaderActions = (): JSX.Element => {
 
   return (
     <section className="flex h-full items-center">
-      <button
+      <div
         onClick={() => { setNewTaskModalIsOpen(!newTaskModalIsOpen) }}
-        className="text-[#FFE5B1] relative mx-3 bg-[#2B3441] p-3 rounded-full text-lg"
+        className="text-[#FFE5B1] relative mx-3 cursor-pointer bg-[#2B3441] p-3 rounded-full text-lg"
       >
         <AiOutlinePlus />
         <div className={`absolute ${newTaskModalIsOpen ? "block" : "hidden"} rounded-sm right-0 w-[16rem] shadow-md py-2 px-4 bg-[#2B3441] top-[112%]`}>
           <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
-            <Image 
+            <Image
               src={`/icons/create-new.svg`}
               width={40}
               height={40}
@@ -38,7 +38,7 @@ const HeaderActions = (): JSX.Element => {
           </button>
 
           <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
-            <Image 
+            <Image
               src={`/icons/create-team.svg`}
               width={40}
               height={40}
@@ -57,7 +57,7 @@ const HeaderActions = (): JSX.Element => {
           </button>
 
           <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
-            <Image 
+            <Image
               src={`/icons/create-team.svg`}
               width={40}
               height={40}
@@ -75,27 +75,144 @@ const HeaderActions = (): JSX.Element => {
             </div>
           </button>
         </div>
-      </button>
+      </div>
 
-      <button
-        className="text-[#FFE5B1] relative mx-3 px-4 py-2 text-xl"
+      <div
+        className="text-[#FFE5B1] cursor-pointer relative mx-3 px-4 py-2 text-xl"
         onClick={() => { setNotificationModalIsOpen(!notificationModalIsOpen) }}
       >
         <IoNotificationsOutline />
 
         <div className={`absolute ${notificationModalIsOpen ? "block" : "hidden"} rounded-sm right-0 w-[16rem] shadow-md py-4 px-4 bg-[#2B3441] top-[112%]`}>
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/create-new.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[55px]`}
+            />
 
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Create Task
+              </p>
+              <p className={`text-[#FFFFFF80] w-full text-left text-sm`}>
+                Create your task now
+              </p>
+            </div>
+          </button>
+
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/create-team.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[55px]`}
+            />
+
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Create Team
+              </p>
+              <p className={`text-[#FFFFFF80] w-full text-left text-sm`}>
+                Create your team now
+              </p>
+            </div>
+          </button>
+
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/create-team.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[55px]`}
+            />
+
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Create Board
+              </p>
+              <p className={`text-[#FFFFFF80] w-full text-left text-sm`}>
+                Create your board now
+              </p>
+            </div>
+          </button>
         </div>
-      </button>
+      </div>
 
       <div
         onClick={() => { setProfileModalIsOpen(!profileModalIsOpen) }}
-        className="px-4 py-2 relative rounded-full bg-[#8CAAB9] mx-3 text-black text-lg"
+        className="px-4 py-2 cursor-pointer relative rounded-full bg-[#8CAAB9] mx-3 text-black text-lg"
       >
         C
 
-        <div className={`absolute ${profileModalIsOpen ? "block" : "hidden"} rounded-sm right-0 w-[16rem] shadow-md py-4 px-4 bg-[#2B3441] top-[112%]`}>
+        <div className={`absolute ${profileModalIsOpen ? "block" : "hidden"} rounded-sm right-0 w-[14rem] overflow-x-hidden shadow-md py-2 px-2 bg-[#2B3441] top-[112%]`}>
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/user-icon.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[40px]`}
+            />
 
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                My Profile
+              </p>
+            </div>
+          </button>
+
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/help.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[40px]`}
+            />
+
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Help and Support
+              </p>
+            </div>
+          </button>
+
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/invite.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[40px]`}
+            />
+
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Invite Friend
+              </p>
+            </div>
+          </button>
+
+          <button className={`flex space-x-2 items-center w-full py-3 px-1 bg-transparent`}>
+            <Image
+              src={`/icons/logout.svg`}
+              width={40}
+              height={40}
+              alt="Create New Icon"
+              className={`w-[40px]`}
+            />
+
+            <div className={`w-full`}>
+              <p className={`text-white w-full font-bold text-left text-base`}>
+                Logout
+              </p>
+            </div>
+          </button>
         </div>
       </div>
     </section>
