daaf47e57584bd98df6f4d57d65e8b672caa801c
Praisecodes <cmancoder@gmail.com>
2023-10-01 23:50:41 +0100

    [fix] Fix responsiveness on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 const Page = (): JSX.Element => {
   return (
     <main className="bg-gradient-to-r from-[#474948] to-[#29313b]">
-      <div className="px-12 pt-10">
-        <Navbar />
+      <div className="lg:px-12 px-4 pt-10">
+        <Navbar /> <br />
         <div>
-          <h1 className="text-[70px] text-white text-center pt-8">
+          <h1 className="lg:text-[70px] text-[30px] text-white text-center pt-8">
             The Easiest Way To
           </h1>
 
-          <h1 className="text-[70px] text-white text-center text-gradient-to-r from-[#FFE5B1] to-[#8CAAB9] mt-[-15px] fontWeight-[700]" style={{ backgroundClip: 'text', WebkitBackgroundClip: 'text', color: 'transparent', backgroundImage: 'linear-gradient(90deg, #FFE5B1, #8CAAB9)' }}>Manage Your Task</h1>
+          <h1 className="lg:text-[70px] text-[40px] text-white text-center text-gradient-to-r from-[#FFE5B1] to-[#8CAAB9] mt-[-15px] fontWeight-[700]" style={{ backgroundClip: 'text', WebkitBackgroundClip: 'text', color: 'transparent', backgroundImage: 'linear-gradient(90deg, #FFE5B1, #8CAAB9)' }}>Manage Your Task</h1>
 
-          <p className="pt-3 text-[20px] text-white text-center">
+          <p className="pt-3 lg:text-[20px] text-[16px] text-white text-center">
             Dentask allows you to manage your task and also gives you opportunity<br />
             to mingle with team members
           </p>
