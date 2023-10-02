e8fd63dc99e949f8664955fc32e5952285778ebe
Praisecodes <cmancoder@gmail.com>
2023-10-02 13:15:51 +0100

    [update] Finish responsiveness for Password Settings page

--- src/app/(user_area)/settings/password/page.tsx
+++ src/app/(user_area)/settings/password/page.tsx
@@ -7,21 +7,21 @@ import { SettingsBoard } from "@/components/templates";
 const Page = (): JSX.Element => {
   return (
     <SettingsBoard title="Password Settings">
-      <div className={`w-[65%] max-w-full`}>
+      <div className={`lg:w-[65%] w-full max-w-full`}>
         <label className={`font-bold text-sm`}>Current Password</label>
         <PasswordInput
           placeholder="Enter password"
         />
       </div> <br />
 
-      <div className={`w-[65%] max-w-full`}>
+      <div className={`lg:w-[65%] w-full max-w-full`}>
         <label className={`font-bold text-sm`}>New Password</label>
         <PasswordInput
           placeholder="Enter New Password"
         />
       </div> <br />
 
-      <div className={`w-[65%] max-w-full`}>
+      <div className={`lg:w-[65%] w-full max-w-full`}>
         <label className={`font-bold text-sm`}>Confirm Password</label>
         <PasswordInput
           placeholder="Confirm New Password"
