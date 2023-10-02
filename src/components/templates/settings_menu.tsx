9681e3b83ab324173f1f26ec3d2d3d1a4b83a73c
Praisecodes <cmancoder@gmail.com>
2023-10-02 12:47:52 +0100

    [update] Add navbar for settings page

--- src/components/templates/settings_menu.tsx
+++ src/components/templates/settings_menu.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { PiGearDuotone, PiLockSimpleBold, PiBellDuotone } from 'react-icons/pi';
+import { AiOutlineBars } from 'react-icons/ai';
 
 const SettingsMenu = (): JSX.Element => {
   const pathname = usePathname();
+  const [navOpen, setNavOpen] = useState<boolean>(false);
 
   const [settingsMenus] = useState<any[]>([
     {
@@ -28,15 +30,39 @@ const SettingsMenu = (): JSX.Element => {
 
   return (
     <section className={`lg:w-[22vw] w-full`}>
-      <h2 className={`text-2xl text-white`}>
-        Settings
-      </h2>
-      <p className={`text-[#ffffff55] text-sm py-1`}>
-        You can find all settings here
-      </p>
+      <div className={`flex items-center justify-between`}>
+        <section>
+          <h2 className={`lg:text-2xl text-xl text-white`}>
+            Settings
+          </h2>
+          <p className={`text-[#ffffff55] text-sm py-1`}>
+            You can find all settings here
+          </p>
+        </section>
+
+        {/** Mobile Nav Bar for settings page */}
+        <div className={`relative`}>
+          <button onClick={() => { setNavOpen(!navOpen) }} className={`text-2xl text-white p-3`}>
+            <AiOutlineBars />
+          </button>
+
+          <div onClick={() => { setNavOpen(!navOpen) }} className={`absolute ${navOpen ? "h-fit py-1" : "h-0 py-0"} space-y-4 top-[90%] px-3 w-[15rem] overflow-hidden right-3 bg-[#2B3441] rounded-md duration-200`}>
+            {settingsMenus.map((settingsMenu, index) => (
+              <Link key={index} href={settingsMenu.route}>
+                <p className={`flex rounded-sm w-full items-center ${settingsMenu.route == pathname && 'bg-[#475464] text-white'} text-[#FFFFFF55] my-2 py-3 text-sm`}>
+                  <span className={`text-base mx-3`}>
+                    {settingsMenu.icon}
+                  </span>
+                  {settingsMenu.name}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
 
       <div className={`lg:w-full w-0 duration-150 overflow-hidden lg:h-fit h-0`}>
-        {settingsMenus.map((settingsMenu, index)=>(
+        {settingsMenus.map((settingsMenu, index) => (
           <Link key={index} href={settingsMenu.route}>
             <p className={`flex rounded-sm w-[75%] items-center ${settingsMenu.route == pathname && 'bg-[#475464] text-white'} text-[#FFFFFF55] my-4 py-3 text-base`}>
               <span className={`text-lg mx-3`}>
