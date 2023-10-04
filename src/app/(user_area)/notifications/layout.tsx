1e25e82e816833028f4de6da3a53fc342f587257
Praisecodes <cmancoder@gmail.com>
2023-10-04 01:18:29 +0100

    [fix] fix notifications page responsiveness

--- src/app/(user_area)/notifications/layout.tsx
+++ src/app/(user_area)/notifications/layout.tsx
@@ -2,10 +2,10 @@ const Layout = ({ children }: { children: React.ReactNode; }): React.ReactNode =
   return (
     <>
       <br />
-      <div className={`mx-auto w-[45rem] max-w-full border py-2 border-[#8CAAB94D] px-6 rounded-lg min-h-[25rem]`}>
+      <div className={`mx-auto w-[45rem] max-w-full border py-2 border-[#8CAAB94D] lg:px-6 px-3 rounded-lg min-h-[25rem]`}>
         <header className={`border-b border-b-[#8CAAB91A] py-3`}>
           <h2 className={`text-white font-bold text-lg`}>
-            Notification
+            Notifications
           </h2>
           <p className={`text-[#FFFFFF80] text-sm`}>
             You can find all notifications here
