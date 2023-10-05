4197a9e8a87b8e97af41de57d28cdfd8a5cca668
Praisecodes <cmancoder@gmail.com>
2023-10-05 13:53:04 +0100

    [fix] fix issue #12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@ import Image from "next/image";
 
 const Page = (): JSX.Element => {
   return (
-    <main className="bg-gradient-to-r font-epilogue from-[#474948] to-[#29313b]">
+    <main className="bg-gradient-to-r min-h-screen font-epilogue from-[#474948] to-[#29313b]">
       <div className="lg:px-12 px-4 pt-10">
         <Navbar /> <br />
         <div>
-          <h1 className="lg:text-[70px] font-sans-expanded text-[30px] text-white text-center pt-8">
+          <h1 className="lg:text-[70px] font-sans-expanded text-[30px] text-white text-center pt-8 pb-3">
             The Easiest Way To
           </h1>
 
-          <h1 className="lg:text-[70px] text-[40px] text-white text-center text-gradient-to-r from-[#FFE5B1] to-[#8CAAB9] mt-[-15px] fontWeight-[700]" style={{ backgroundClip: 'text', WebkitBackgroundClip: 'text', color: 'transparent', backgroundImage: 'linear-gradient(90deg, #FFE5B1, #8CAAB9)' }}>Manage Your Task</h1>
+          <h1 className="lg:text-[70px] text-[40px] text-white text-center text-gradient-to-r from-[#FFE5B1] to-[#8CAAB9] mt-[-15px] fontWeight-[700]" style={{ backgroundClip: 'text', WebkitBackgroundClip: 'text', color: 'transparent', backgroundImage: 'linear-gradient(90deg, #FFE5B1, #8CAAB9)' }}>
+            Manage Your Tasks As A Student
+          </h1>
 
           <p className="pt-3 lg:text-[20px] text-[16px] text-white text-center">
             Dentask allows you to manage your task and also gives you opportunity<br />
             to mingle with team members
           </p>
         </div>
-        <div className="items-center mt-10 lg:w-4/5 w-full m-auto">
+        <div className="flex items-center justify-center self-baseline mt-10 lg:w-4/5 w-full mx-auto">
           <Image width={900} height={900} src="/images/Dashboard.png" alt="Logo" className={`w-full object-cover object-center`} />
         </div>
 
--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <header className="flex justify-between items-center">
       <div>
-        <Image width={100} height={100} src="/images/logo_var.svg" alt="Logo" className={`lg:w-[12rem] w-[8rem] object-cover object-center`} />
+        <Image width={100} height={100} src="/images/logo_var.svg" alt="Logo" className={`lg:w-[12rem] w-[7rem] object-cover object-center`} />
       </div>
 
-      <Link href={`/signup`} className=" text-white border-[#FFE5B1] font-sans-expanded border-2 rounded-md bg-[#858173] px-8 py-3 ">
+      <Link href={`/signup`} className="lg:text-base text-sm text-white border-[#FFE5B1] font-sans-expanded border-2 rounded-md bg-[#858173] px-5 py-2 lg:px-8 lg:py-3 ">
         Get Started
       </Link>
     </header>
